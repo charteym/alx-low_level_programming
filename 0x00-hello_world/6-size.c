3dde47aeb3ebfc88fa4333962e500b76ab86d436
charteym <charteym@gmail.com>
2023-03-16 13:48:09 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)", sizeof(char)\n);
+	printf("Size of an int: %lu byte(s)", sizeof(int)\n);
+	printf("Size of a long int; %lu byte(s)", sizeof(long int)\n);
+	printf("Size of a long long int; %lu bytes(s)", sizeof(long long int)\n);
+	printf("Size of a float; %lu byte(s)", sizeof(float)\n);
+	return (0);
+}
