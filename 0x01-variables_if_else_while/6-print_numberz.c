318831bff140980b0090701f6611c48b5f222113
charteym <charteym@gmail.com>
2023-03-17 12:00:18 -0700

    single numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: Prints all single digit number of base 10
+ * starting from 0, followed by new line
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		c++
+	}
+
+	putchar('\n');
+	return (0);
+}
