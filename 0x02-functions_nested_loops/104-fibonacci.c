d57da427e8bbd98365be899a8bc41cabb1882540
charteym <charteym@gmail.com>
2023-03-21 16:34:22 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,20 +7,37 @@
  */
 int main(void)
 {
-	int count;
-	unsigned long fib1 = 1, fib2 = 2, sum;
+	unsigned long int i;
+	unsigned long int bef = 1;
+	unsigned long int aft = 2;
+	unsigned long int l = 1000000000;
+	unsigned long int bef1;
+	unsigned long int bef2;
+	unsigned long int aft1;
+	unsigned long int aft2;
 
-	printf("%lu, %lu", fib1, fib2);
+	printf("%lu", bef);
 
-	for (count = 2; count < 98; count++)
+	for (i = 1; i < 91; i++)
 	{
-		sum = fib1 + fib2;
-		printf(", %lu", sum);
+		printf(", %lu", aft);
+		aft += bef;
+		bef = aft - bef;
+	}
+	bef1 = (bef / l);
+	bef2 = (bef % l);
+	aft1 = (aft / l);
+	aft2 = (aft % l);
 
-		fib1 = fib2;
-		fib2 = sum;
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", aft1 + (aft2 / l));
+		printf("%lu", aft2 % l);
+		aft1 = aft1 + bef1;
+		bef1 = aft1 - bef1;
+		aft2 = aft2 + bef2;
+		bef2 = aft2 - bef2;
 	}
 	printf("\n");
-
 	return (0);
 }
