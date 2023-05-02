cc6076bd140394468cc0a818cd9abc449476951f
charteym <charteym@gmail.com>
2023-05-02 10:05:13 -0700

    C - More singly linked lists

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a linked listint_t list
- * @h: pointer to the head node of the list
+ * listint_len - returns the number of elements in a linked lists
+ * @h: linked list of type listint_t to traverse
  *
- * Return: the number of nodes
+ * Return: number of nodes
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t count = 0;
+	size_t num = 0;
 
-	while (h != NULL)
-
+	while (h)
 	{
+		num++;
 		h = h->next;
-		count++;
 	}
-	return (count);
+
+	return (num);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h> /* for size_t */
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -9,14 +10,13 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * for alx project
  */
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
-
-/* Function prototypes */
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -34,4 +34,4 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-#endif /* LISTS_H */
+#endif
